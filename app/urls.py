d080806c36017d0107a781e0842059070568c4aa
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-07-07 00:17:30 +0300

    Registered simple tag for categories

--- app/urls.py
+++ app/urls.py
@@ -18,9 +18,10 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 
-from app.settings import DEBUG
+from app import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +29,6 @@ urlpatterns = [
     path("catalog/", include("goods.urls", namespace="catalog")),
 ]
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
