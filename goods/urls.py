e9869545313fd270525ace1b2a1f9aae7c897e4b
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-07-08 18:18:47 +0300

    Simple search created

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = "goods"
 
 urlpatterns = [
+    path("search/", catalog, name="search"),
     path("<slug:category_slug>/", catalog, name="index"),
     path("product/<slug:product_slug>/", product, name="product"),
 ]
