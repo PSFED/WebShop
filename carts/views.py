dd2df9135a0c26cb52cbbc4cb656f62652484c13
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-07-11 20:25:46 +0300

    Cart remove controller added

--- carts/views.py
+++ carts/views.py
@@ -20,15 +20,14 @@ def cart_add(request, product_slug):
 
     return redirect(request.META["HTTP_REFERER"])
 
-    context = {}
-    return render(request, ".html", context)
-
 
 def cart_change(request, product_slug):
     context = {}
     return render(request, ".html", context)
 
 
-def cart_remove(request, product_slug):
-    context = {}
-    return render(request, ".html", context)
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+
+    return redirect(request.META["HTTP_REFERER"])
