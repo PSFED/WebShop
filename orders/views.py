4ed80f286973c9963d9fcbdd18c108b64c1ff2af
Pavel Semenov <x.pavel.semenov.x@gmail.com>
2024-07-15 15:40:07 +0300

    Final edits

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,3 @@
-from email import message
 from django.contrib import messages
 from django.db import transaction
 from django.forms import ValidationError
@@ -6,8 +5,10 @@ from django.shortcuts import redirect, render
 from carts.models import Cart
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def create_order(request):
     if request.method == "POST":
         form = CreateOrderForm(data=request.POST)
@@ -67,5 +68,6 @@ def create_order(request):
     context = {
         "title": "Home - Оформление заказа",
         "form": form,
+        "order": True,
     }
     return render(request, "orders/create_order.html", context)
